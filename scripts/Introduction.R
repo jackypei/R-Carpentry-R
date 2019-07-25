2221c4940e2239d73136ebda088ea943efbad1c2
JackyPei <tianmi.pei@sydney.edu.au>
2019-07-25 15:48:50 +1000

    added a sqrt function, and rounded to 2 dp

--- scripts/Introduction.R
+++ scripts/Introduction.R
@@ -7,3 +7,33 @@ install.packages("tidyverse")
 print("Hello world")
 
 library(tidyverse)
+
+area_hectares <- 1.0
+(1.0->area_hectares)
+
+
+area_hectares <- 50                     #land area in hectares
+area_acres <- 2.47*area_hectares        #convert hectares to acres
+area_acres                              #print
+
+
+
+length <- 3.8
+width <- 2.1 
+area <- length*width
+area
+
+
+length <- 103
+width <- 59
+area <- length*width
+area
+
+a <- 64
+b <- sqrt(a)
+b
+
+
+round(3.141592,2)
+
+
